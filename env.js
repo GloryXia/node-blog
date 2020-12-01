61752e474c6e46638830c6e32d2af393ae685159
xiachao <xiachao@qgutech.com>
2020-12-01 21:43:37 +0800

    服务器地址

--- env.js
+++ env.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 // 例如生产环境可以换成你的阿里云地址
-const host = isProd? 'localhost' : 'localhost'
-const port = isProd? '8888' : '8080'
+const host = isProd? 'www.xiachao.site' : 'localhost'
+const port = isProd? '80' : '8080'
 
 module.exports = {
     host,
