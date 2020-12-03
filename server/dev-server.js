b6f47342f9d32493207497f19d6ca819e75fbeaa
xiachao <xiachao@qgutech.com>
2020-12-03 18:02:34 +0800

    SSR代码阅读

--- server/dev-server.js
+++ server/dev-server.js
@@ -40,7 +40,8 @@ function render(req, res) {
     const context = {
         url: req.url
     }
-
+    // 这里无需传入一个应用程序，因为在执行 bundle 时已经自动创建过。
+    // 现在我们的服务器与应用程序已经解耦！
     renderer.renderToString(context, (err, html) => {
         if (err) return handleError(err)
         res.send(html)
